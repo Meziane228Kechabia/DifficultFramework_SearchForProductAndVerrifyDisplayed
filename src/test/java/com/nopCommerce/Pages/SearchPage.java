2c021dfd60a92b94532c7cbb09b7e0354fe72881
meziane228@gmail.com <meziane228@gmail.com>
2024-07-28 21:59:46 -0500

    Updating

---
+++ src/test/java/com/nopCommerce/Pages/SearchPage.java
@@ -0,0 +1,48 @@
+package com.nopCommerce.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SearchPage {
+public WebDriver driver;
+
+@FindBy(xpath="//input[@id='small-searchterms']")
+private WebElement searchBox;
+
+@FindBy(xpath="//button[@type='submit']")
+private WebElement searchButton;
+
+//@FindBy(linkText = "Apple MacBook Pro 13-inch")
+//private WebElement appleProductDetails;
+
+@FindBy(linkText = "Apple MacBook Pro 13-inch")
+private WebElement validProduct;
+
+public SearchPage (WebDriver driver) {
+	this.driver = driver;
+	PageFactory.initElements(driver, this);
+}
+//public SearchPage clickOnBooksHomePageIcon() {
+//	searchField.click();
+//	return new SearchPage(driver);
+//}
+public void enterValidProductName(String validProductText) {
+	searchBox.sendKeys(validProductText);
+}
+public SearchPage clickOnSearchButton() {
+	searchButton.click();
+	return new SearchPage(driver);
+}
+//public SearchPage clickOnProductToAccessDeatils() {
+//	appleProductDetails.click();
+//	return new SearchPage(driver);
+//}
+public boolean verifyValidProductPresence () {
+	boolean displayStatus = validProduct.isDisplayed();
+	return displayStatus;
+}
+
+	
+}
---
+++ src/test/java/com/nopCommerce/TestCases/SearchValidProduct.java
@@ -0,0 +1,40 @@
+package com.nopCommerce.TestCases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.nopCommerce.Pages.SearchPage;
+import com.nopCommerce.TestBase.TestBase;
+
+
+public class SearchValidProduct extends TestBase {
+	public WebDriver driver;
+	public SearchPage searchPage;	
+	
+	public SearchValidProduct() throws Exception{ 
+		  super();
+	}
+@BeforeMethod
+public void checkoutSetup() {
+  	driver = openingApplicationWithAnyBrowser(prop.getProperty("browser"));
+  	searchPage = new SearchPage(driver);
+	
+}
+@Test(priority=1)
+public void searchAndNavigateToValidProduct () {
+	searchPage.enterValidProductName(dataProp.getProperty("validProduct"));
+	searchPage.clickOnSearchButton();	
+// this is the first  option to assert the presence of the product is displayed(more direct)
+		Assert.assertTrue(searchPage.verifyValidProductPresence());		
+// this is another option to assert the presence of the product
+	//	String actualProductName = "Apple MacBook Pro 13-inch";
+	//	String expectedProductName = driver.findElement(By.linkText("Apple MacBook Pro 13-inch")).getText();		
+	//	assertTrue(actualProductName.contains(expectedProductName), "Product name does not match!");
+	//	searchPage.clickOnProductToAccessDeatils();
+}
+public void tearDown() {
+	driver.quit();
+}
+}
